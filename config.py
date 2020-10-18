fce8f224e170333a7e2284333f46ac399ce43aeb
59-29 <64929036+59-29@users.noreply.github.com>
2020-10-18 21:53:25 +0400

    first edit

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 TOKEN = "NzY3NDIyNDY1NjQ2Mzk1NDI0.X4xr3Q.bW3QCQnQ5vm3ydYpAaOtbuwxYr4"
 
 # Postgres DSN for the bot's database. It's needed for the bot to be able to store data in a SQL database. It looks like this: "postgres://[user]:[password]@[host]:[port]/[database name]" (Some options may not be needed)
-POSTGRES = "Placeholder text"
+POSTGRES = "postgres://u41z:Ahmed2908@localhost:5432/mydb"
