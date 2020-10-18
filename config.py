8cae8b9afc19a754da3451774ac810462f1ceab7
59-29 <64929036+59-29@users.noreply.github.com>
2020-10-18 21:12:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Discord bot token
-TOKEN = "Placeholder text"
+TOKEN = "NzY3NDIyNDY1NjQ2Mzk1NDI0.X4xr3Q.bW3QCQnQ5vm3ydYpAaOtbuwxYr4"
 
 # Postgres DSN for the bot's database. It's needed for the bot to be able to store data in a SQL database. It looks like this: "postgres://[user]:[password]@[host]:[port]/[database name]" (Some options may not be needed)
 POSTGRES = "Placeholder text"
